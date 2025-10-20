6ea4cb7627d06eb90a6a25c8d85d04ec2f1b0c6a
kwwall <kevin.w.wall@gmail.com>
2025-10-20 00:55:02 -0400

    Test GHAS secrets scanning with phone creds

---
+++ src/test/java/phony-s3cr3ts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Unreal secrets
+
+prodPassword="fake Cr3d$"
+
+cat <<-!!!SSH_KEY!!! > $HOME/.ssh/super_important_ssh_key
+	-----BEGIN OPENSSH PRIVATE KEY-----
+	b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAaAAAABNlY2RzYS
+	1zaGEyLW5pc3RwMjU2AAAACG5pc3RwMjU2AAAAQQSZ8MkTeIlFZ8Wg6DyGzdUcn6Jc1XUM
+	M2lDl0n38sDMb4o37VcdfT28gpA44drAAl3kwr/TXKuNwDc0BsHWrKIrAAAAqEwC3SZMAt
+	0mAAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJnwyRN4iUVnxaDo
+	PIbN1RyfolzVdQwzaUOXSffywMxvijftVx19PbyCkDjh2sACXeTCv9Ncq43ANzQGwdasoi
+	sAAAAhANnUrZ+O0vavy+alZ++RxqH7tzKV6QomHeuHvMBUka7+AAAADXdhbGxrQGZleW5t
+	YW4BAg==
+	-----END OPENSSH PRIVATE KEY-----
+!!!SSH_KEY!!!
+
+curl --dry-run -u fake_user:$prodPassword https://example.com
+

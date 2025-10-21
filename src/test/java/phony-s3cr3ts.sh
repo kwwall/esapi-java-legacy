99e588d95f3585b4057080175b864588c5811b27
kwwall <kevin.w.wall@gmail.com>
2025-10-21 17:30:42 -0400

    Minor dumb tweaks for testing.

--- src/test/java/phony-s3cr3ts.sh
+++ src/test/java/phony-s3cr3ts.sh
@@ -2,11 +2,11 @@
 # Unreal secrets - fake password and SSH private key to see if GHAS will
 # detectthem.
 
-prodPassword="fake Cr3d$"
+prodPassword="fa|<e Cr3d$"
 
 cat <<-!!!SSH_KEY!!! > $HOME/.ssh/super_important_ssh_key
 	-----BEGIN OPENSSH PRIVATE KEY-----
-	b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAaAAAABNlY2RzYS
+	b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQBAAAAAAAABAAAAaAAAABNlY2RzYS
 	1zaGEyLW5pc3RwMjU2AAAACG5pc3RwMjU2AAAAQQSZ8MkTeIlFZ8Wg6DyGzdUcn6Jc1XUM
 	M2lDl0n38sDMb4o37VcdfT28gpA44drAAl3kwr/TXKuNwDc0BsHWrKIrAAAAqEwC3SZMAt
 	0mAAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJnwyRN4iUVnxaDo
